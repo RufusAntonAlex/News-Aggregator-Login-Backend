762059eb0719a4fc6585b989f27309b7a413cad9
Ajjay Adhithya V <156827226+ajjay0604@users.noreply.github.com>
2024-07-18 20:03:15 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 // User.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true }, // Added email field
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }
 });
 
 // Create and export the User model based on the schema
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
